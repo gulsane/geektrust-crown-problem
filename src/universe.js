02f96997526da40939bc68e0812ea482753a1972
gulsane <craftygulsane@gmail.com>
2021-02-18 14:09:11 +0530

    Added: addKingdom method to class Universe

--- src/universe.js
+++ src/universe.js
@@ -8,6 +8,10 @@ class Universe {
 	doesKingdomExists(name) {
 		return this.kingdoms.some((kingdom) => kingdom.kingdomName === name);
 	}
+
+	addKingdom({ name, emblem }) {
+		this.kingdoms.push(new Kingdom(name, emblem));
+	}
 }
 
 module.exports = { Universe };
--- test/testUniverse.js
+++ test/testUniverse.js
@@ -2,11 +2,27 @@ const assert = require("chai").assert;
 
 const { Universe } = require("../src/universe");
 
-describe.only("#Universe", () => {
+describe("#Universe", () => {
 	describe("doesKingdomExists", () => {
+		const universe = new Universe();
+		const kingdom = { name: "zooland", emblem: "animal" };
+		universe.addKingdom(kingdom);
+
 		it("Should return false if kingdom not exists", () => {
+			assert.isFalse(universe.doesKingdomExists("wrong name"));
+		});
+
+		it("Should return true if kingdom exists", () => {
+			assert.isTrue(universe.doesKingdomExists(kingdom.name));
+		});
+	});
+
+	describe("addKingdom", () => {
+		it("Should add the kingdom to the list of universe", () => {
 			const universe = new Universe();
-			assert.isFalse(universe.doesKingdomExists("LAND"));
+			const kingdom = { name: "LAND", emblem: "OWL" };
+			universe.addKingdom(kingdom);
+			assert.isTrue(universe.doesKingdomExists(kingdom.name));
 		});
 	});
 });
